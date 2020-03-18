6610215747f41b67432a55cfe4fabe628c1bb143
razonyang <razonyang@gmail.com>
2020-03-18 10:19:06 +0800

    Update app.js and remove unused js and css

--- themes/frontend/static/js/app.js
+++ themes/frontend/static/js/app.js
@@ -2,4 +2,21 @@ $.ajaxSetup({
     beforeSend: function (xhr) {
         xhr.setRequestHeader("X-CSRF-Token",  $('input[name="gorilla.csrf.Token"]').val())
     }
-});
+})
+
+$(function() {
+    var lang = Cookies.get("lang")
+    var langSelector = $('#langSelector')
+    langSelector.find('a[data-id="' + lang + '"]').addClass('active')
+    langSelector.find('a').on('click', function() {
+        langSelector.find('a').removeClass('active')
+        var self = $(this)
+        self.addClass('active')
+        Cookies.set("lang", self.attr("data-id"))
+        window.location.reload()
+    })
+
+    if (!navigator.cookieEnabled) {
+        message("Cookie", "Cookie was disabled by your browser", "error")
+    }
+})
