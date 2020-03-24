0366c7e20bb0a4feee1b30ee6496211d85013c3c
razonyang <razonyang@gmail.com>
2020-03-24 09:43:31 +0800

    Update contact form

--- internal/frontend/controllers/site.go
+++ internal/frontend/controllers/site.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"fmt"
-	"net/http"
 
 	"github.com/clevergo/clevergo"
 	"github.com/clevergo/demo/internal/core"
@@ -29,25 +28,16 @@ func (s *Site) About(ctx *clevergo.Context) error {
 }
 
 func (s *Site) Contact(ctx *clevergo.Context) error {
-	captcha, err := s.CaptcpaManager().Generate()
-	if err != nil {
-		return err
-	}
-
-	form := forms.NewContact(s.Mailer(), s.CaptcpaManager())
 	if ctx.IsPost() {
-		if err = form.Handle(ctx); err == nil {
-			s.AddFlash(ctx, bootstrap.NewSuccessAlert("Thanks for contacting us, we'll get in touch with you as soon as possible."))
-			ctx.Redirect("/contact", http.StatusFound)
-			return nil
+		form := forms.NewContact(s.Mailer(), s.CaptcpaManager())
+		if err := form.Handle(ctx); err != nil {
+			s.Logger().Error(err)
+			return jsend.Error(ctx.Response, err.Error())
 		}
-		s.AddFlash(ctx, bootstrap.NewDangerAlert(err.Error()))
-		s.Logger().Error(err)
+		s.AddFlash(ctx, bootstrap.NewSuccessAlert("Thanks for contacting us, we'll get in touch with you as soon as possible."))
+		return jsend.Success(ctx.Response, nil)
 	}
 	return s.Render(ctx, "site/contact", core.ViewData{
-		"form":    form,
-		"error":   err,
-		"captcha": captcha,
 	})
 }
 
